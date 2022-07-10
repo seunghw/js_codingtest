105a715225e3cab6bbd829259fcd16405055a3f5
Seunghwan <ysp1307@naver.com>
2022-07-10 23:22:34 +0900

    feat: 삼각형 결정조건

--- 섹션1/2. 삼각형 판별하기.js
+++ 섹션1/2. 삼각형 판별하기.js
@@ -1,5 +1,27 @@
+// 내가 작성한 답.
+
 function solution(a, b, c) {
+  let data = [a, b, c].sort((a, b) => a - b);
+
+  if (data[2] < data[0] + data[1]) {
+    answer = "YES";
+  } else answer = "NO";
+
   return answer;
 }
 
 console.log(solution(13, 33, 17));
+
+// 정답
+
+// function solution(a, b, c){
+//   let answer="YES", max;
+//   let tot=a+b+c;
+//   if(a>b) max=a;
+//   else max=b;
+//   if(c>max) max=c;
+//   if(tot-max<=max) answer="NO";
+//   return answer;
+// }
+
+// console.log(solution(13, 33, 17));
