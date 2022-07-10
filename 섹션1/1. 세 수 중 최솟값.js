5ad586eb7f9de9e54063d0ebb01f4f96c8cc52fe
Seunghwan <ysp1307@naver.com>
2022-07-10 23:22:14 +0900

    feat: 최솟값 비교

--- 섹션1/1. 세 수 중 최솟값.js
+++ 섹션1/1. 세 수 중 최솟값.js
@@ -1,6 +1,26 @@
+// 내가 작성한 답
+
 function solution(a, b, c) {
   let answer;
 
+  if (a < b) {
+    answer = a;
+  } else answer = b;
+
+  if (c < answer) answer = c;
+
+  return answer;
+}
+
+console.log(solution(2, 5, 1));
+
+// 정답
+
+function solution(a, b, c) {
+  let answer;
+  if (a < b) answer = a;
+  else answer = b;
+  if (c < answer) answer = c;
   return answer;
 }
 
