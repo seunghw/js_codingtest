b69c10efd9afd8b6822c347f14e902a22092e55b
Seunghwan <ysp1307@naver.com>
2022-07-21 22:10:18 +0900

    feat: 2sol. 대문자 체크할 때 아스키 활용 또는 toUpperCase() 활용하기

--- 섹션1/11. 대문자 찾기.js
+++ 섹션1/11. 대문자 찾기.js
@@ -1,4 +1,10 @@
 function solution(s) {
+  let answer = 0;
+  for (x of s) {
+    if (x === x.toUpperCase()) answer++; // 내가작성한 코드
+    // let num = x.charCodeAt(); // 독특한 해답 아스키.  charCodeAt 활용
+    // if (num >= 65 && num <= 90) answer++; //
+  }
   return answer;
 }
 
