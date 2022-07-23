57e7ad851eace5815eda9af73a5673e25311f627
Seunghwan <ysp1307@naver.com>
2022-07-23 22:15:58 +0900

    feat: 3sol. indexOf 중복제거

--- 섹션1/16. 중복문자제거.js
+++ 섹션1/16. 중복문자제거.js
@@ -1,4 +1,12 @@
 function solution(s) {
+  let answer = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (i === s.indexOf(s[i])) {
+      answer += s[i];
+    }
+  }
+
   return answer;
 }
 console.log(solution("ksekkset"));
