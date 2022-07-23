dc96201ea522c856491d8eda66ecb478ea63d606
Seunghwan <ysp1307@naver.com>
2022-07-23 22:20:52 +0900

    feat: 4sol. filter, indexOf 활용 중복단어제거

--- 섹션1/17. 중복단어제거.js
+++ 섹션1/17. 중복단어제거.js
@@ -1,5 +1,7 @@
 function solution(s) {
-  return answer;
+  return s.filter((a, i) => {
+    if (s.indexOf(a) === i) return true;
+  });
 }
 let str = ["good", "time", "good", "time", "student"];
 console.log(solution(str));
