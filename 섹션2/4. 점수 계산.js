295bb41d937004c46f7406d3c1606eab1ba96c14
Seunghwan <ysp1307@naver.com>
2022-07-24 16:37:45 +0900

    feat: 연속된 수 가산점으로 체크

--- 섹션2/4. 점수 계산.js
+++ 섹션2/4. 점수 계산.js
@@ -1,6 +1,15 @@
 function solution(arr) {
-  let answer = 0,
-    cnt = 0;
+  let answer = 0;
+  let cnt = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 1) {
+      cnt++;
+      answer += cnt;
+    } else {
+      cnt = 0;
+    }
+  }
 
   return answer;
 }
