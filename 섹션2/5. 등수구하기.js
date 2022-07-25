4bd1e36808de9ac234dc9a4804160b0fc325a69e
Seunghwan <ysp1307@naver.com>
2022-07-25 23:48:42 +0900

    feat: 2차원배열로 등수 구하기

--- 섹션2/5. 등수구하기.js
+++ 섹션2/5. 등수구하기.js
@@ -1,4 +1,12 @@
 function solution(arr) {
+  let answer = Array.from({ length: arr.length }, () => 1);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[i]) answer[i]++;
+    }
+  }
+
   return answer;
 }
 
