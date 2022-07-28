3b4b65cb9383f89eb26583751d7992545525717a
Seunghwan <ysp1307@naver.com>
2022-07-28 23:09:20 +0900

    feat: replace, 정규식 활용

--- 섹션3/3. 숫자만 추출.js
+++ 섹션3/3. 숫자만 추출.js
@@ -1,4 +1,7 @@
-function solution(str) {}
+function solution(str) {
+  str = str.toUpperCase().replace(/[A-Z]/g, "");
+  return Number(str);
+}
 
 let str = "g0en2T0s8eSoft";
 console.log(solution(str));
