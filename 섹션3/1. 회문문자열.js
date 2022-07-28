d18ee2f2c6d0d099873ce99cbc76841ef1d7fb10
Seunghwan <ysp1307@naver.com>
2022-07-28 21:04:18 +0900

    feat: 회문 찾는법 두가지. reverse, 맨앞 맨뒤 비교

--- 섹션3/1. 회문문자열.js
+++ 섹션3/1. 회문문자열.js
@@ -3,7 +3,12 @@ function solution(s) {
 
   s = s.toUpperCase();
 
-  if (s.split("").reverse().join("") != s) answer = "NO";
+  //   if (s.split("").reverse().join("") != s) answer = "NO";  뒤집어서 체크
+
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    // 절반까지 돌려서 맨앞과 맨 뒤를 체크
+    if (s[i] != s[s.length - i - 1]) return "NO";
+  }
 
   return answer;
 }
