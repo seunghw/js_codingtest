133ef95f5213e3f933dab7b9f8aa2ae859e0bd2c
Seunghwan <ysp1307@naver.com>
2022-07-28 23:51:08 +0900

    feat: 문자거리 구할땐 반복문 앞뒤로 돌리면서 체크

--- 섹션3/4. 가장 짧은 문자거리.js
+++ 섹션3/4. 가장 짧은 문자거리.js
@@ -1,5 +1,52 @@
 function solution(s, t) {
+  let data = 10000;
+  let answer = [data];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === t) {
+      data = 0;
+      answer[i] = data;
+    } else {
+      data++;
+      answer[i] = data;
+    }
+  }
+
+  for (let j = s.length - 1; j >= 0; j--) {
+    if (s[j] === t) {
+      data = 0;
+    } else {
+      data++;
+    }
+
+    if (answer[j] > data) {
+      answer[j] = data;
+    }
+  }
+
   return answer;
+
+  //   let answer=[];  //답지 정답
+  //   let p=1000;
+  //   for(let x of s){
+  //       if(x===t){
+  //           p=0;
+  //           answer.push(p);
+  //       }
+  //       else{
+  //           p++;
+  //           answer.push(p);
+  //       }
+  //   }
+  //   p=1000;
+  //   for(let i=s.length-1; i>=0; i--){
+  //       if(s[i]===t) p=0;
+  //       else{
+  //           p++;
+  //           answer[i]=Math.min(answer[i], p);
+  //       }
+  //   }
+  //   return answer;
 }
 
 let str = "teachermode";
